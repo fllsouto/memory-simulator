63456c13bbae47ea2c6ff5a8156cc81df0974263
Pedro Scocco <pedroscocco@gmail.com>
2015-10-03 13:19:04 -0300

    Fixed indentation from tabs to spaces

--- lib/process_manager.rb
+++ lib/process_manager.rb
@@ -3,16 +3,16 @@ require "awesome_print"
 
 class ProcessManager
 
-	attr_accessor :event_queue
+  attr_accessor :event_queue
 
-	Event = Struct.new(:type, :time, :name, :pid, :size, :address)
+  Event = Struct.new(:type, :time, :name, :pid, :size, :address)
 
   def initialize
     @trace_file = nil
-		@event_queue = []
+    @event_queue = []
   end
 
-	@@instance = ProcessManager.new
+  @@instance = ProcessManager.new
 
   def self.instance
     return @@instance
@@ -25,36 +25,36 @@ class ProcessManager
   def set_trace_file path
     @trace_file = path
 
-		parse_trace_file path
+    parse_trace_file path
   end
 
-	def parse_trace_file path
-		trace_file = File.new(path, 'r')
-		trace_file.gets #discard first line
-		index = 0
-		while ((line = trace_file.gets) != nil) do
-			args = line.split
-			# t0 nome tf b p1 t1 p2 t2 p3 t3 [pn tn]
+  def parse_trace_file path
+    trace_file = File.new(path, 'r')
+    trace_file.gets #discard first line
+    index = 0
+    while ((line = trace_file.gets) != nil) do
+      args = line.split
+      # t0 nome tf b p1 t1 p2 t2 p3 t3 [pn tn]
 
-			time = args.shift.to_i
+      time = args.shift.to_i
 
-			name = args.shift
-			pid = index += 1
+      name = args.shift
+      pid = index += 1
 
-			finish = args.shift.to_i
+      finish = args.shift.to_i
 
-			size = args.shift.to_i
-			@event_queue << Event.new('start_process', time, name, pid, size)
+      size = args.shift.to_i
+      @event_queue << Event.new('start_process', time, name, pid, size)
 
-			@event_queue << Event.new('finish_process', finish, name, pid)
+      @event_queue << Event.new('finish_process', finish, name, pid)
 
-			while args.size > 1 do
-				address, time = args.shift(2).map(&:to_i)
+      while args.size > 1 do
+        address, time = args.shift(2).map(&:to_i)
 
-				@event_queue << Event.new('memory_access', time, name, pid, nil, address)
-			end
-		end
+        @event_queue << Event.new('memory_access', time, name, pid, nil, address)
+      end
+    end
 
-		@event_queue.sort { |a, b| a.time <=> b.time}
-	end
+    @event_queue.sort { |a, b| a.time <=> b.time}
+  end
 end
